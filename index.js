0d2e531485f788fa4da6af88e3b8c14c0cc3ab1d
dulcefabiola <dulcefabiola@gmail.com>
2021-12-14 17:43:45 -0600

    axios

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 //2.MIDDLEWARES
 //BAse de Datos
 connectDB();
 //Todas las peticioes y respuesta se manejan en protocolo JSON
+//permite flexibilizar el servidor para que acceda un server externo
+app.use(cors());
 app.use(express.json());
 
 //3.RUTAS
