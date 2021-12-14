c7cdaecf63f265ea08af154b783df2316960287f
dulcefabiola <dulcefabiola@gmail.com>
2021-12-14 12:19:26 -0600

    integracion de JWT

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,18 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("./../controllers/userController");
+const authorization = require("./../middleware/authorization");
 
 // 2. ROUTER
+//CREAR USUARIO
 router.post("/create", userController.create);
+
+//INICIAR SESION DE USUARIO
+router.post("/login", userController.login);
+
+//VERIFICACON DE USUARIO
+//Sucede a traves de un metodo get
+router.get("/verifytoken", authorization, userController.verifyToken);
 
 // 3. EXPORTACIÓN
 module.exports = router;
